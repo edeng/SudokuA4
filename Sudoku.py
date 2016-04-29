8d52cf2377b8489f369018a5ba94d3d3e1e50108
Eden Ghirmai <edeng94@gmail.com>
2016-04-28 20:24:13 -0700

    initial commit: skeleton

---
+++ Sudoku.py
@@ -0,0 +1,38 @@
+''' Sudoku.py
+Luxun Lu & Eden Ghirmai 
+CSE415 Spr16 - 4/29/2015
+Assignment 4: Problem Formulation
+'''
+
+#<METADATA>
+PROBLEM_NAME = "Sudoku"
+PROBLEM_AUTHORS = ['Eden Ghirmai', 'Luxun Lu']
+PROBLEM_CREATION_DATE = "29-APR-2016"
+PROBLEM_DESC=\
+'''This formulation of the basic eight puzzle uses generic
+Python 3 constructs and has been tested with Python 3.4.
+It is designed to work according to the QUIET tools interface.
+'''
+
+#<COMMON_CODE>
+#</COMMON_CODE>
+
+#<COMMON_DATA>
+#</COMMON_DATA>
+
+#<INITIAL_STATE>
+#</INITIAL_STATE>
+
+#<OPERATORS>
+#</OPERATORS>
+
+#<GOAL_TEST>
+#</GOAL_TEST>
+
+#<GOAL_MESSAGE_FUNCTION> (optional)
+#</GOAL_MESSAGE_FUNCTION>
+
+#<STATE_VIS>
+#</STATE_VIS>
+
+
